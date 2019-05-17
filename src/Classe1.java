d01d9207383186119e65dcfd26e8bb54deeb9732
Yassine <Yassine>
2019-05-17 13:46:38 +0200

    creation de l'arborescence projet

---
+++ src/Classe1.java
@@ -0,0 +1,4 @@
+public class Classe1 {
+	public static void main(String[] args) {
+		system.out.println("Hello world!");}
+}
---
+++ src/Classe2.java
@@ -0,0 +1,3 @@
+public class Classe2 {
+	public static void main(String[] args) {
+		system.out.println("coucou!");}
